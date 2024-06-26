9981660eb3aa585a7717c7938c9c9735aaf5c1a4
Sébastien Gaudard <sebastien.gaudard.1@gmail.com>
2024-06-26 12:16:57 +0200

    fix: removed unused

--- client/src/components/Datatable/PhoneSheetCall.tsx
+++ client/src/components/Datatable/PhoneSheetCall.tsx
@@ -1,35 +1,4 @@
 import { ScrollArea } from "../ui/scroll-area"
-import { Separator } from "../ui/separator"
-
-const tags = Array.from({ length: 50 }).map(
-  (_, i, a) => `v1.2.0-beta.${a.length - i}`
-)
-
-const calls = [{
-  date: "2021-08-30 14h32",
-  title: "Notification opération J-1",
-  messages: [
-    "Notification de l'hoptital de Paris",
-    "Vous avez rendez-vous pour votre opération le 31/08/2021 à 14h30",
-    "Merci de venir à jeun",
-  ]
-}, {
-  date: "2021-09-02 14h30",
-  title: "Notification opération J+2",
-  messages: [
-    "Notification de l'hoptital de Paris",
-    "Nous esperons que votre rétablissement se passe bien",
-    "Si vous avez des douleurs ou des questions, n'hésitez pas à nous contacter",
-  ]
-}, {
-  date: "2021-09-06 14h30",
-  title: "Notification opération J+5",
-  messages: [
-    "Notification de l'hoptital de Paris",
-    "Nous esperons que votre rétablissement s'est bien passé",
-    "Si vous avez des retours ou des commentaires, n'hésitez pas à nous contacter",
-  ]
-}];
 
 export type Call = {
   id: string
