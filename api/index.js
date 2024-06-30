d0fcb59bed7f863ad99128d4b84451bbbaa3e57f
Soheib HDF <s.hadef@outlook.fr>
2024-06-30 22:46:12 +0200

    masquage clef api

--- api/index.js
+++ api/index.js
@@ -4,6 +4,7 @@ import cors from 'cors';
 import bodyParser from 'body-parser';
 import { ObjectId } from 'mongodb';
 import axios from 'axios';
+import dotenv from 'dotenv';
 
 const app = express();
 const port = process.env.APP_PORT ?? 3000;
@@ -69,8 +70,8 @@ app.post('/emulate-outbound-call', async (req, res) => {
 app.post('/send-voice-message', async (req, res) => {
   const { recipientPhoneNumber, audioUrl } = req.body;
 
-  const ACCESS_TOKEN = 'EAALa9J2mdwwBO8n4GswdAyyijzEAIqpiX2wk9UdifsiiO2BnkpMpP389iWcIleX8Coi0g0CaZCAFDqwOA0OoQSKI4K8yKjvIg0ZBEembT2HHqPbN7cbyvdTZAsj2di50LdJJXkPPdChJ95JUZAnAhFOuOfWU3ryI1cbH0B9LUaw4jz0kVAYuz76nvQKW3uhFg21dwlpDwt2PPNj2Ww7MCZAy8GefZCcBkG';
-  const PHONE_NUMBER_ID = '299765513230571';
+  const ACCESS_TOKEN = process.env.ACCESS_TOKEN;
+  const PHONE_NUMBER_ID = process.env.PHONE_NUMBER_ID;
 
   const url = `https://graph.facebook.com/v13.0/${PHONE_NUMBER_ID}/messages`;
 
